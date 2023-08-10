1151cf971c3d5aed5ff88e03aa64a147e9dc0b69
Hamed <hamid.alfati77ha@gmail.com>
2023-08-10 15:05:13 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * print a line using function puts
- * main -print the sentence
- * return 0 -correct output
+ * print a sentence  using function puts
+ * main - print the sentence
+ * return 0 - correct output
  */
 
 int main(void)
